9c7cd6d82e150392791c7d1588be61fe0cd51042
floholzer <Florentin.Holzer@gmx.at>
2023-06-28 19:33:59 +0200

    inital commit

---
+++ src/test/java/at/technikum/slmstringmodify/SlmStringModifyApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmstringmodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmStringModifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
